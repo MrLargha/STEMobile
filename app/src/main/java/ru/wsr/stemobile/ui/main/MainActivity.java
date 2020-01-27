e7b9a25e997682101753e864e97b28c3d419f0a0
MrLargha <cppcodery@gmail.com>
2020-01-27 23:19:50 +0300

    Added splash-screen

--- app/src/main/java/ru/wsr/stemobile/ui/main/MainActivity.java
+++ app/src/main/java/ru/wsr/stemobile/ui/main/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
